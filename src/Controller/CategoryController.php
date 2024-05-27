d3bb3f8205a971f789e88cb32e797a506320992b
D-CYL <118185691+D-CYL@users.noreply.github.com>
2024-05-27 15:07:30 +0200

    category page is up and functional
    just need to insert information in the database and complete the routing in the twig files.

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,12 +11,20 @@ use Symfony\Component\Routing\Attribute\Route;
 class CategoryController extends AbstractController
 {
     #[Route('/category', name: 'app_category')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-
-
+        $categories = $entityManager->getRepository(Category::class)->findAll();
         return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
+            'categories' => $categories,
+        ]);
+    }
+
+    #[Route('/category/{category}', name: 'app_genre')]
+    public function category(EntityManagerInterface $entityManager): Response
+    {
+        $categories = $entityManager->getRepository(Category::class)->findAll();
+        return $this->render('category/genre.html.twig', [
+            'categories' => $categories,
         ]);
     }
 }
