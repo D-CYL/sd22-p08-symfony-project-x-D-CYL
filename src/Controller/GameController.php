577d7ea52e5b6048aadb3fb981070abb40f0ee76
D-CYL <118185691+D-CYL@users.noreply.github.com>
2024-05-27 12:54:08 +0200

    game pages routing done
    just need to fill the cards and add the category to them later on.

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Game;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,17 +11,20 @@ use Symfony\Component\Routing\Attribute\Route;
 class GameController extends AbstractController
 {
     #[Route('/game', name: 'app_game')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $game = $entityManager->getRepository(Game::class)->findAll();
         return $this->render('game/index.html.twig', [
-            'controller_name' => 'GameController',
+            'game' => $game,
         ]);
     }
     #[Route('/game/{game}', name: 'app_game_info')]
-    public function game(): Response
+    public function game(EntityManagerInterface $entityManager, $game): Response
     {
+
+        $info = $entityManager->getRepository(Game::class)->find($game);
         return $this->render('game/info.html.twig', [
-            'controller_name' => 'GameController',
+            'info' => $info,
         ]);
     }
 }
